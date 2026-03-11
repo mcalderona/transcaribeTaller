0d0a70b172acf881161960198090a040d2ec6b89
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-10 22:38:22 -0500

    feat: clase vehiculo con su constructor, get y metodos

--- src/main/java/org/example/modelo/Vehiculo.java
+++ src/main/java/org/example/modelo/Vehiculo.java
@@ -5,5 +5,36 @@ public abstract class Vehiculo implements Imprimible {
     protected String Ruta;
     protected int CapacidadMaxima;
     protected int PasajerosActuales;
-    
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima) {
+        this.Placa = placa;
+        this.Ruta = ruta;
+        this.CapacidadMaxima = capacidadMaxima;
+        this.PasajerosActuales = 0;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public String getRuta() {
+        return Ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return CapacidadMaxima;
+    }
+
+    public int getPasajerosActuales() {
+        return PasajerosActuales;
+    }
+    public int  getCuposDisponibles(){ //metodo que calcula cuántos cupos quedan disponibles en el vehículo
+        return CapacidadMaxima - PasajerosActuales;
+    }
+
+     public void SubirPasajeros(){  //metodo que permite subir un pasajero al vehículo, pero solo si aún hay espacio
+        if (PasajerosActuales < CapacidadMaxima ){
+            PasajerosActuales++;
+        }
+     }
 }
