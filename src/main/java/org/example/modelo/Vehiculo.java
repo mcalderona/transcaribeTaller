08a6015e7e13967dbfddf9ef1820607bd23f42fa
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-10 22:47:48 -0500

    feat: clase abstrata vehiculo con los metodos, get y constructor

--- src/main/java/org/example/modelo/Vehiculo.java
+++ src/main/java/org/example/modelo/Vehiculo.java
@@ -9,5 +9,32 @@ public abstract class Vehiculo implements Imprimible {
     protected boolean disponible;
     protected double tarifaBase;
 
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, double tarifaBase) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.tarifaBase = tarifaBase;
+        this.pasajerosActuales = 0;
+        this.disponible = true;
+    }
+
+    public int getCuposDisponibles() {
+        return capacidadMaxima - pasajerosActuales;
+    }
+
+    public boolean ocuparCupo() {
+        if (pasajerosActuales < capacidadMaxima) {
+            pasajerosActuales++;
+            return true;
+        }
+        return false;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public abstract String getTipo();
+
 }
 
