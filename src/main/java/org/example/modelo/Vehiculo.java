01c083a7a00938a92353b4acb9e8359c9892db12
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-10 22:46:03 -0500

    feat: clase abstracta vehiculo creada con los atributos

--- src/main/java/org/example/modelo/Vehiculo.java
+++ src/main/java/org/example/modelo/Vehiculo.java
@@ -1,40 +1,13 @@
 package org.example.modelo;
 
 public abstract class Vehiculo implements Imprimible {
-    protected String Placa;
-    protected String Ruta;
-    protected int CapacidadMaxima;
-    protected int PasajerosActuales;
 
-    public Vehiculo(String placa, String ruta, int capacidadMaxima) {
-        this.Placa = placa;
-        this.Ruta = ruta;
-        this.CapacidadMaxima = capacidadMaxima;
-        this.PasajerosActuales = 0;
-    }
+    protected String placa;
+    protected String ruta;
+    protected int capacidadMaxima;
+    protected int pasajerosActuales;
+    protected boolean disponible;
+    protected double tarifaBase;
 
-    public String getPlaca() {
-        return Placa;
-    }
-
-    public String getRuta() {
-        return Ruta;
-    }
-
-    public int getCapacidadMaxima() {
-        return CapacidadMaxima;
-    }
-
-    public int getPasajerosActuales() {
-        return PasajerosActuales;
-    }
-    public int  getCuposDisponibles(){ //metodo que calcula cuántos cupos quedan disponibles en el vehículo
-        return CapacidadMaxima - PasajerosActuales;
-    }
-
-     public void SubirPasajeros(){  //metodo que permite subir un pasajero al vehículo, pero solo si aún hay espacio
-        if (PasajerosActuales < CapacidadMaxima ){
-            PasajerosActuales++;
-        }
-     }
 }
+
