549f784c5a8549774148650bce54b416ba5ff5cc
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-11 20:32:19 -0500

    feat: constantes que guardan nombres de archivos

--- src/main/java/org/example/dao/VehiculoDAO.java
+++ src/main/java/org/example/dao/VehiculoDAO.java
@@ -1,5 +1,9 @@
 package org.example.dao;
 
 public class VehiculoDAO {
+    //constantes que guardan los nombres de los archivos donde se almacenan los datos
+    private static final String BUSETA_FILE = "buseta.txt";
+    private static final String MICROBUES_FILE = "microbus.txt";
+    private static final String BUS_FILE = "bus.txt";
     
 }
