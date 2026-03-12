c695b448b3c41e4f097fcd2459b853df7c520a6b
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-11 21:09:32 -0500

    feat: lista almacenadora y objeto dao creado

--- src/main/java/org/example/servicio/VehiculoServicio.java
+++ src/main/java/org/example/servicio/VehiculoServicio.java
@@ -1,5 +1,17 @@
 package org.example.servicio;
 
+import org.example.dao.VehiculoDAO;
+import org.example.modelo.Vehiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehiculoServicio {
-    
+    private List<Vehiculo> vehiculos = new ArrayList<>();
+    private VehiculoDAO dao = new VehiculoDAO();
+
+
+
+
+
 }
