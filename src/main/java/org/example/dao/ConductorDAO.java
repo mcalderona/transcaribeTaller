62f662eaa8053b33a08bd9f4eaad6aa242aa1aab
Jose Chinchia <jachinchia@unicesar.edu.co>
2026-03-12 14:19:51 -0500

    Feat: Clase ConductorDAO

---
+++ src/main/java/org/example/dao/ConductorDAO.java
@@ -0,0 +1,41 @@
+package org.example.dao;
+import org.example.modelo.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConductorDAO {
+
+    private String ARCHIVOS = "pasajeros.txt";
+
+    public void guardar(Conductor c) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(ARCHIVOS, true))) {
+            writer.write(c.getCedula() + ";" + c.getNombre() + ";" + c.getCategorialicencia() + ";" + c.getNumerolicencia());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error al guardar conductor: " + e.getMessage());
+        }
+    }
+
+    public List<Conductor> cargarTodos() {
+        List<Conductor> lista = new ArrayList<>();
+        File archivo = new File(ARCHIVOS);
+        if (!archivo.exists()) return lista;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (!linea.trim().isEmpty()) {
+                    String[] campos = linea.split(";");
+                    Conductor c = new Conductor(campos[1], Integer.parseInt(campos[2]), campos[3], campos[4]);
+                    lista.add(c);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al cargar conductores: " + e.getMessage());
+        }
+
+        return lista;
+    }
+}
