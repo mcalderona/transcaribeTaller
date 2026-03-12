ede90fc633a29ed30592527d2816147e20d3ccef
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-11 20:18:46 -0500

    feat: clase MicroBus con el constructor y los metodos

--- src/main/java/org/example/modelo/MicroBus.java
+++ src/main/java/org/example/modelo/MicroBus.java
@@ -5,4 +5,23 @@ public class MicroBus extends Vehiculo{
     private static final int   CAPACIDAD = 25;
     private static final double  TARIFA = 10000;
 
+    public MicroBus(String placa, String ruta) {
+        super(placa, ruta, CAPACIDAD, TARIFA);
+    }
+
+
+    @Override
+    public String getTipo() {
+        return "MicroBus";
+    }
+
+    @Override
+    public void ImprimirDetalle() {
+
+        System.out.println("Tipo: MicroBus");
+        System.out.println("Placa: " + placa);
+        System.out.println("Ruta: " + ruta);
+        System.out.println("Cupos disponibles: " + getCuposDisponibles());
+        System.out.println("Tarifa: " + tarifaBase);
+    }
 }
