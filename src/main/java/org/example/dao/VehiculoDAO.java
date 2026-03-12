065a31a9ec673ad1435101d53b17c0bd94d72370
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-11 20:41:45 -0500

    feat: metodo que guarda un vehiculo en un archivo creado

--- src/main/java/org/example/dao/VehiculoDAO.java
+++ src/main/java/org/example/dao/VehiculoDAO.java
@@ -1,9 +1,26 @@
 package org.example.dao;
 
+import org.example.modelo.Vehiculo;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class VehiculoDAO {
     //constantes que guardan los nombres de los archivos donde se almacenan los datos
     private static final String BUSETA_FILE = "buseta.txt";
     private static final String MICROBUES_FILE = "microbus.txt";
     private static final String BUS_FILE = "bus.txt";
-    
+
+    public void GuardarVehiculo(Vehiculo v) {
+        String Archivo =ObtenerArchivo(v.getTipo());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(Archivo, true))) {
+            bw.write(v.placa + "," + v.ruta);
+            bw.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
