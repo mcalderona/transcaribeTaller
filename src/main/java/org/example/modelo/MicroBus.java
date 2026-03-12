f9ebaabf92f8b56ee89c10f784749912cbf4a2c3
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-03-11 20:15:03 -0500

    feat: variables privadas , MicroBus hereda de vehiculo

--- src/main/java/org/example/modelo/MicroBus.java
+++ src/main/java/org/example/modelo/MicroBus.java
@@ -1,5 +1,8 @@
 package org.example.modelo;
 
-public class MicroBus {
-    
+public class MicroBus extends Vehiculo{
+
+    private static final int   CAPACIDAD = 25;
+    private static final double  TARIFA = 10000;
+
 }
