e9899673dded79496ba4aa30a6413b235b3163d7
Jose Chinchia <jachinchia@unicesar.edu.co>
2026-03-12 15:31:39 -0500

    Feat: Clase Ticket

---
+++ src/main/java/org/example/modelo/Ticket.java
@@ -0,0 +1,72 @@
+package org.example.modelo;
+
+public class Ticket {
+
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private String fechaCompra;
+    private String origen;
+    private String destino;
+    private double valorFinal;
+
+    public Ticket() {
+    }
+
+    public Ticket(Pasajero pasajero, Vehiculo vehiculo, String fechaCompra, String origen, String destino) {
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaCompra = fechaCompra;
+        this.origen = origen;
+        this.destino = destino;
+        this.valorFinal = calcularTotal();
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero; }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo; }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }public void setFechaCompra(String FechaCompra) {
+        this.fechaCompra = FechaCompra;}
+
+    public String getOrigen() {
+        return origen;
+    }public void setOrigen(String origen) {
+        this.origen = origen;}
+
+    public String getDestino() {
+        return destino;
+    }public void setDestino(String Destino) {
+        this.destino = Destino;}
+
+    public double getValorFinal() {
+        return valorFinal;
+    }public void setValorFinal(double ValorFinal) {
+        this.valorFinal = ValorFinal;}
+
+    @Override
+    public double calcularTotal() {
+        double tarifaBase = vehiculo.getTarifaBase();
+        double descuento = pasajero.CalcularDescuento();
+        return tarifaBase - (tarifaBase * descuento);
+    }
+
+    @Override
+    public String imprimirDetalle() {
+        return  "\nFecha de Compra: " + fechaCompra +
+                "\nOrigen: " + origen +
+                "\nDestino: " + destino +
+                "\nPasajero: " + pasajero.getNombre() +
+                "\nVehículo: " + vehiculo.getplaca() +
+                "\nValor Final: $" + valorFinal;
+    }
+
+
+}
